56e6b1f66ccf7fbb6d565c5e8ecf6ee6bcaeab97
hhoang168 <hhoang168@gmail.com>
2020-10-21 17:09:08 +0700

    Add checkin checkout getallTicket getTicketByBikeID getCheckoutTicketById

--- chaincode/java/src/main/java/com/fptu/paa/ParkingContract.java
+++ chaincode/java/src/main/java/com/fptu/paa/ParkingContract.java
@@ -55,24 +55,22 @@ public class ParkingContract implements ContractInterface {
         ChaincodeStub stub = ctx.getStub();
         System.out.println("Calling Init Function");
         //Init sample data
-        String[] ticketData = {
-                "{ \"bikeID\": \"1\", \"ownerCheckInID\": \"1\", \"checkinTime\": \"17/10/2020-08:58:51:958\", \"nfcNumber\": \"\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
-                "{ \"bikeID\": \"2\", \"ownerCheckInID\": \"1\", \"checkinTime\": \"17/10/2020-09:58:51:958\", \"nfcNumber\": \"\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
-                "{ \"bikeID\": \"3\", \"ownerCheckInID\": \"1\", \"checkinTime\": \"17/10/2020-10:58:51:958\", \"nfcNumber\": \"\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
-                "{ \"bikeID\": \"4\", \"ownerCheckInID\": \"2\", \"checkinTime\": \"17/10/2020-11:58:51:958\", \"nfcNumber\": \"\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
-                "{ \"bikeID\": \"5\", \"ownerCheckInID\": \"2\", \"checkinTime\": \"17/10/2020-12:58:51:958\", \"nfcNumber\": \"\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
-                "{ \"bikeID\": \"\", \"ownerCheckInID\": \"1\", \"checkinTime\": \"17/10/2020-13:58:51:958\", \"nfcNumber\": \"123456789\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
-                "{ \"bikeID\": \"\", \"ownerCheckInID\": \"1\", \"checkinTime\": \"17/10/2020-14:58:51:958\", \"nfcNumber\": \"123456788\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
-                "{ \"bikeID\": \"\", \"ownerCheckInID\": \"1\", \"checkinTime\": \"17/10/2020-15:58:51:958\", \"nfcNumber\": \"123456786\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
-                "{ \"bikeID\": \"\", \"ownerCheckInID\": \"2\", \"checkinTime\": \"17/10/2020-16:58:51:958\", \"nfcNumber\": \"123456785\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
-                "{ \"bikeID\": \"\", \"ownerCheckInID\": \"2\", \"checkinTime\": \"17/10/2020-17:58:51:958\", \"nfcNumber\": \"123456782\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}"
-        };
-//        int i = 0;
-        for (String data : ticketData) {
-            Ticket ticket = genson.deserialize(data, Ticket.class);
-//            String ticketKey = String.format("TICKET%d", ++i);
-            createTicket(ctx, ticket.getBikeID(), ticket.getNfcNumber(), ticket.getOwnerCheckInID(), ticket.getCheckinTime(), ticket.getCheckinImages()[0], ticket.getCheckinImages()[1]);
-        }
+//        String[] ticketData = {
+//                "{ \"bikeID\": \"1\", \"ownerCheckInID\": \"1\", \"checkinTime\": \"17/10/2020-08:58:51:958\", \"nfcNumber\": \"\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
+//                "{ \"bikeID\": \"2\", \"ownerCheckInID\": \"1\", \"checkinTime\": \"17/10/2020-09:58:51:958\", \"nfcNumber\": \"\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
+//                "{ \"bikeID\": \"3\", \"ownerCheckInID\": \"1\", \"checkinTime\": \"17/10/2020-10:58:51:958\", \"nfcNumber\": \"\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
+//                "{ \"bikeID\": \"4\", \"ownerCheckInID\": \"2\", \"checkinTime\": \"17/10/2020-11:58:51:958\", \"nfcNumber\": \"\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
+//                "{ \"bikeID\": \"5\", \"ownerCheckInID\": \"2\", \"checkinTime\": \"17/10/2020-12:58:51:958\", \"nfcNumber\": \"\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
+//                "{ \"bikeID\": \"\", \"ownerCheckInID\": \"1\", \"checkinTime\": \"17/10/2020-13:58:51:958\", \"nfcNumber\": \"123456789\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
+//                "{ \"bikeID\": \"\", \"ownerCheckInID\": \"1\", \"checkinTime\": \"17/10/2020-14:58:51:958\", \"nfcNumber\": \"123456788\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
+//                "{ \"bikeID\": \"\", \"ownerCheckInID\": \"1\", \"checkinTime\": \"17/10/2020-15:58:51:958\", \"nfcNumber\": \"123456786\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
+//                "{ \"bikeID\": \"\", \"ownerCheckInID\": \"2\", \"checkinTime\": \"17/10/2020-16:58:51:958\", \"nfcNumber\": \"123456785\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}",
+//                "{ \"bikeID\": \"\", \"ownerCheckInID\": \"2\", \"checkinTime\": \"17/10/2020-17:58:51:958\", \"nfcNumber\": \"123456782\",\"checkinImages\":[\"idPlateImage\",\"idFaceImage\"]}"
+//        };
+//        for (String data : ticketData) {
+//            Ticket ticket = genson.deserialize(data, Ticket.class);
+//            createTicket(ctx, ticket.getBikeID(), ticket.getNfcNumber(), ticket.getOwnerCheckInID(), ticket.getCheckinTime(), ticket.getCheckinImages()[0], ticket.getCheckinImages()[1]);
+//        }
     }
 
     @Transaction()
@@ -151,49 +149,52 @@ public class ParkingContract implements ContractInterface {
         args.add(checkOutBikeImage);
         args.add(checkOutFaceImage);
         if (checkArgs(args)) {
-            System.out.println("CHECKOUT BIKE:" + ticketKey);
             //Create stub
             ChaincodeStub stub = ctx.getStub();
             //Get ticket
             String ticketState = stub.getStringState(ticketKey);
             Ticket ticket = genson.deserialize(ticketState, Ticket.class);
-            //Begin checkout
-            String[] checkOutImages = {checkOutBikeImage, checkOutFaceImage};
-            Ticket newTicket = new Ticket(ticket.getBikeID(), ticket.getOwnerCheckInID(), ticket.getCheckinTime(), ownerCheckOutID,
-                    checkOutTime, ticket.getNfcNumber(), paymentType,
-                    ticket.getCheckinImages(), checkOutImages, TicketStatus.FINISH.name());
-            //Store check out ticket
-            String newTicketState = genson.serialize(newTicket);
-            System.out.println("NEW State: " + newTicketState);
-            stub.putStringState(ticketKey, newTicketState);
+            //Verified if this bike is parking
+            if (ticket.getStatus().equals(TicketStatus.KEEPING)) {
+                System.out.println("CHECKOUT BIKE:" + ticketKey);
+                //Begin checkout
+                String[] checkOutImages = {checkOutBikeImage, checkOutFaceImage};
+                Ticket newTicket = new Ticket(ticket.getBikeID(), ticket.getOwnerCheckInID(), ticket.getCheckinTime(), ownerCheckOutID,
+                        checkOutTime, ticket.getNfcNumber(), paymentType,
+                        ticket.getCheckinImages(), checkOutImages, TicketStatus.FINISH.name());
+                //Store check out ticket
+                String newTicketState = genson.serialize(newTicket);
+                System.out.println("NEW State: " + newTicketState);
+                stub.putStringState(ticketKey, newTicketState);
 
-            //Begin update index key
-            boolean isNFC = false;
-            if (newTicket.getNfcNumber() != null && !newTicket.getNfcNumber().isEmpty()) {
-                isNFC = true;
+                //Begin update index key
+                boolean isNFC = false;
+                if (newTicket.getNfcNumber() != null && !newTicket.getNfcNumber().isEmpty()) {
+                    isNFC = true;
+                }
+                //step 1: remove old key
+                if (!isNFC) {
+                    String bikeStatusTicketIndexKey = stub.createCompositeKey(IndexName.BIKE_STATUS_TICKET.name(), TicketStatus.KEEPING.name(), newTicket.getBikeID(), ticketKey).toString();
+                    System.out.println("Checkin Bike Old Key - " + bikeStatusTicketIndexKey);
+                    stub.putStringState(bikeStatusTicketIndexKey, "");
+                    //step 2: insert new key
+                    bikeStatusTicketIndexKey = stub.createCompositeKey(IndexName.BIKE_STATUS_TICKET.name(), TicketStatus.FINISH.name(), newTicket.getBikeID(), ticketKey).toString();
+                    System.out.println("Checkout Bike New Key - " + bikeStatusTicketIndexKey);
+                    stub.putStringState(bikeStatusTicketIndexKey, DEFAULT_VALUE);
+                } else {
+                    String nfcStatusTicketIndexKey = stub.createCompositeKey(IndexName.NFC_STATUS_TICKET.name(), TicketStatus.KEEPING.name(), newTicket.getNfcNumber(), ticketKey).toString();
+                    System.out.println("Checkin NFC Old Key - " + nfcStatusTicketIndexKey);
+                    stub.putStringState(nfcStatusTicketIndexKey, "");
+                    //step 2: insert new key
+                    nfcStatusTicketIndexKey = stub.createCompositeKey(IndexName.NFC_STATUS_TICKET.name(), TicketStatus.FINISH.name(), newTicket.getNfcNumber(), ticketKey).toString();
+                    System.out.println("Checkout NFC New Key - " + nfcStatusTicketIndexKey);
+                    stub.putStringState(nfcStatusTicketIndexKey, DEFAULT_VALUE);
+                }
+                //Return checkout ticket
+                result = newTicketState;
+                System.out.println("FINISH CHECKOUT");
             }
-            //step 1: remove old key
-            if (!isNFC) {
-                String bikeStatusTicketIndexKey = stub.createCompositeKey(IndexName.BIKE_STATUS_TICKET.name(), TicketStatus.KEEPING.name(), newTicket.getBikeID(), ticketKey).toString();
-                System.out.println("Checkin Bike Old Key - " + bikeStatusTicketIndexKey);
-                stub.putStringState(bikeStatusTicketIndexKey, "");
-                //step 2: insert new key
-                bikeStatusTicketIndexKey = stub.createCompositeKey(IndexName.BIKE_STATUS_TICKET.name(), TicketStatus.FINISH.name(), newTicket.getBikeID(), ticketKey).toString();
-                System.out.println("Checkout Bike New Key - " + bikeStatusTicketIndexKey);
-                stub.putStringState(bikeStatusTicketIndexKey, DEFAULT_VALUE);
-            } else {
-                String nfcStatusTicketIndexKey = stub.createCompositeKey(IndexName.NFC_STATUS_TICKET.name(), TicketStatus.KEEPING.name(), newTicket.getNfcNumber(), ticketKey).toString();
-                System.out.println("Checkin NFC Old Key - " + nfcStatusTicketIndexKey);
-                stub.putStringState(nfcStatusTicketIndexKey, "");
-                //step 2: insert new key
-                nfcStatusTicketIndexKey = stub.createCompositeKey(IndexName.NFC_STATUS_TICKET.name(), TicketStatus.FINISH.name(), newTicket.getNfcNumber(), ticketKey).toString();
-                System.out.println("Checkout NFC New Key - " + nfcStatusTicketIndexKey);
-                stub.putStringState(nfcStatusTicketIndexKey, DEFAULT_VALUE);
-            }
-            //Return checkout ticket
-            result = newTicketState;
         }
-        System.out.println("FINISH CHECKOUT");
         return result;
     }
 
--- first-network/ccp-generate.sh
+++ first-network/ccp-generate.sh
@@ -28,6 +28,26 @@ function yaml_ccp {
         ccp-template.yaml | sed -e $'s/\\\\n/\\\n        /g'
 }
 
+function json_ccp_custom {
+    local PP1=$(one_line_pem $5)
+    local CP1=$(one_line_pem $6)
+	local PP2=$(one_line_pem ${11})
+    local CP2=$(one_line_pem ${12})
+    sed -e "s/\${ORG1}/$1/" \
+        -e "s/\${P0PORT1}/$2/" \
+        -e "s/\${P1PORT1}/$3/" \
+        -e "s/\${CAPORT1}/$4/" \
+        -e "s#\${PEERPEM1}#$PP1#" \
+        -e "s#\${CAPEM1}#$CP1#" \
+		-e "s/\${ORG2}/$7/" \
+        -e "s/\${P0PORT2}/$8/" \
+        -e "s/\${P1PORT2}/$9/" \
+        -e "s/\${CAPORT2}/${10}/" \
+        -e "s#\${PEERPEM2}#$PP2#" \
+        -e "s#\${CAPEM2}#$CP2#" \
+        ccp-template-custom.json 
+}
+
 ORG=1
 P0PORT=7051
 P1PORT=8051
@@ -47,3 +67,18 @@ CAPEM=crypto-config/peerOrganizations/org2.example.com/ca/ca.org2.example.com-ce
 
 echo "$(json_ccp $ORG $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" > connection-org2.json
 echo "$(yaml_ccp $ORG $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" > connection-org2.yaml
+
+ORG1=1
+P0PORT1=7051
+P1PORT1=8051
+CAPORT1=7054
+PEERPEM1=crypto-config/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
+CAPEM1=crypto-config/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem
+ORG2=2
+P0PORT2=9051
+P1PORT2=10051
+CAPORT2=8054
+PEERPEM2=crypto-config/peerOrganizations/org2.example.com/tlsca/tlsca.org2.example.com-cert.pem
+CAPEM2=crypto-config/peerOrganizations/org2.example.com/ca/ca.org2.example.com-cert.pem
+
+echo "$(json_ccp_custom $ORG1 $P0PORT1 $P1PORT1 $CAPORT1 $PEERPEM1 $CAPEM1 $ORG2 $P0PORT2 $P1PORT2 $CAPORT2 $PEERPEM2 $CAPEM2)" > connection-custom.json
